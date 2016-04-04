4ed50b966b236683aecc34c9095e3f1292ec4977
Ian Pickering <ipickering2@gmail.com>
2016-04-03 22:47:11 -0500

    rewrite to use tabulated-list

--- sos.el
+++ sos.el
@@ -28,28 +28,36 @@
 (require 'json)
 (require 'url)
 (require 'url-http)
-(require 'org)
+(require 'tabulated-list)
 
 (provide 'sos)
+
+(defvar sos-results)
+
+(defvar sos-question-list-window)
+(defvar sos-answer-view-window)
+
+(defvar sos-question-list-buffer)
 
 (defvar sos-get-answers 'nil
   "If non-nil retrieve and SO's answers to SO's questions when building the search result buffer.
 This will slow down the process.")
 
-(defun sos-decode-html-entities ()
-  "Decodes HTML entities in a buffer."
-  (loop for entity in '(("&quot;" . "\"")
-                        ("&apos;" . "'")
-                        ("&#39;" . "'")
-                        ("&hellip" . "...")
-                        ("&amp;" . "&")
-                        ("&gt;" . ">")
-                        ("&lt;" . "<")
-                        ("&#194;" . "Â")
-                        ("&#178;" . "²"))
-        do (goto-char (point-min))
-        (while (search-forward (car entity) nil t)
-          (replace-match (cdr entity) nil t))))
+(defun sos-decode-html-entities (str)
+  "Decodes HTML entities in a string."
+  (let ((result str))
+    (loop for entity in '(("&quot;" . "\"")
+                          ("&apos;" . "'")
+                          ("&#39;" . "'")
+                          ("&hellip" . "...")
+                          ("&amp;" . "&")
+                          ("&gt;" . ">")
+                          ("&lt;" . "<")
+                          ("&#194;" . "Â")
+                          ("&#178;" . "²"))
+          do (setq result (replace-regexp-in-string (regexp-quote (car entity)) (cdr entity) result nil 'literal))
+          )
+    result))
 
 (defun sos-uncompress-callback (&optional status)
   "Callback for url-retrieve that decompresses gzipped content in
@@ -106,101 +114,236 @@ into the current buffer."
 
 
 ;;;###autoload
-(defun sos (query)
+(defun sos (query tag)
   "Searches StackOverflow for the given `query'. Displays excerpts from the search results.
 
 API Reference: http://api.stackexchange.com/docs/excerpt-search"
-  (interactive "sSearch StackOverflow: ")
+  (interactive "sSearch StackOverflow: \nsTag: ")
   (let* ((api-url (concat "http://api.stackexchange.com/2.2/search/excerpts"
                           "?order=desc"
-                          "&sort=relevance"
+                          "&sort=votes"
+                          "&tagged=" tag
                           "&q=" (url-hexify-string query)
                           "&site=stackoverflow"))
          (response-buffer (url-retrieve-synchronously api-url))
          (json-response (sos-get-response-body response-buffer)))
     ;; set up the buffer
-    (switch-to-buffer (concat "*sos - " query "*"))
+    (switch-to-buffer-other-window (concat "*sos - " query "*"))
+    (setq sos-question-list-window (selected-window))
+    (setq sos-question-list-buffer (current-buffer))
+    (sos-mode)
     (erase-buffer)
-    (org-mode)
+    ;; (org-mode)
     (visual-line-mode t)
-    (insert "#+TITLE: StackOverflow Search: " query "\n")
+    ;; (insert "#+TITLE: StackOverflow Search: " query "\n")
 
-    ;; display the search results
-    (loop for item across (cdr (assoc 'items json-response))
-          do (sos-insert-search-result item))
+    (setq tabulated-list-format
+            (vector
+                    '("title" 80 t)
+                    '("score" 13 t)
+                    ;; '("modules" 30 t)
+                    ;; '("name" 1 t)
+                    ))
 
-    (sos-decode-html-entities)
-    ;; strip out HTML tags
-    (goto-char (point-min))
-    (while (search-forward "<span class=\"highlight\">" nil t)
-      (replace-match "" nil t))
-    (goto-char (point-min))
-    (while (search-forward "</span>" nil t)
-      (replace-match "" nil t))
+    (setq tabulated-list-sort-key nil)
 
-    (goto-char (point-min))
-    (org-global-cycle 1)
-    (sos-mode-on)))
+    (let* ((results (cdr (assoc 'items json-response)))
+           (count (length results))
+            entries)
+
+      (setq sos-results results)
+
+      (dotimes (i count)
+        (let* ((result (elt results i))
+               (result-name (sos-decode-html-entities (cdr (assoc 'title result))))
+               (result-score (int-to-string (cdr (assoc 'score result)))))
+
+          (push (list i (vector
+
+                         result-name
+                         result-score
+
+                         ))
+                entries)))
+      (setq tabulated-list-entries (nreverse entries)))
+    (tabulated-list-init-header)
+    (tabulated-list-print)
+
+    ;; (sos-decode-html-entities)
+    ;; ;; strip out HTML tags
+    ;; (goto-char (point-min))
+    ;; (while (search-forward "<span class=\"highlight\">" nil t)
+    ;;   (replace-match "" nil t))
+    ;; (goto-char (point-min))
+    ;; (while (search-forward "</span>" nil t)
+    ;;   (replace-match "" nil t))
+
+    ;; (goto-char (point-min))
+    ;; (org-global-cycle 1)
+    ))
 
 
 (defun sos-get-answers (id)
   "Get answers for SO's question defined by ID."
   (let* ((api-url (concat "http://api.stackexchange.com/2.2/"
-			  "questions/"
-			  (if (stringp id) id
-			    (int-to-string id))
-			  "/answers"
-			  "?order=desc"
-			  "&sort=activity"
-			  "&filter=withbody"
-			  "&site=stackoverflow"))
-	 (response-buffer (url-retrieve-synchronously api-url))
-	 (json-response (progn
-			  (switch-to-buffer response-buffer)
-			  (goto-char (point-min))
-			  (sos-get-response-body response-buffer)))
-	 (answer-list (cdr (assoc 'items json-response)))
-	 (n-answers (length answer-list))
-	 (i 0)
-	 (sos-string
-	  (concat "\n\n** Answers [" (int-to-string n-answers) "]\n")))
+                          "questions/"
+                          (if (stringp id) id
+                            (int-to-string id))
+                          "/answers"
+                          "?order=desc"
+                          "&sort=activity"
+                          "&filter=withbody"
+                          "&site=stackoverflow"))
+         (response-buffer (url-retrieve-synchronously api-url))
+         (json-response (progn
+                          (switch-to-buffer response-buffer)
+                          (goto-char (point-min))
+                          (sos-get-response-body response-buffer)))
+         (answer-list (cdr (assoc 'items json-response)))
+         (n-answers (length answer-list))
+         (i 0)
+         (sos-string
+          (concat "Answers [" (int-to-string n-answers) "]\n")))
     (while (< i n-answers) 
-      (setq sos-string
-	    (concat sos-string
-		    (concat "\n\n*** Answer no. " (int-to-string (1+ i)) 
-			    "\n"
-			    (cdr (assoc 'body (elt answer-list i)))
-			    "\n"))
-	    i (1+ i)))
+      (let ((answer (elt answer-list i)))
+        (setq sos-string
+              (concat sos-string
+                      (concat "Answer no. " (int-to-string (1+ i)) 
+                              " Score: " (int-to-string (cdr (assoc 'score answer)))
+                              "\n"
+                              (cdr (assoc 'body answer))
+                              "\n"))
+              i (1+ i))))
     sos-string))
+
+(defun sos-current-result ()
+  (elt sos-results (tabulated-list-get-id)))
 
 ;;;###autoload
 (defun sos-answer ()
   "Get answers for SO question ID as defined in property block of the current question."
   (interactive)
-  (let ((id (org-entry-get (point) "ID" t)))
+  (let ((id (cdr (assoc 'question_id (sos-current-result)))))
     (if (not id)
-	(message "Cannot see question ID at point.")
-      (org-narrow-to-subtree)
-      (goto-char (point-max))
-      (insert (sos-get-answers id))
-      (widen)
-      (org-back-to-heading))))
+        (message "Cannot see question ID at point.")
+      (setq sos-answer-view-window
+            (cond ((with-demoted-errors "Unable to split window: %S"
+                     (split-window-vertically 20)))
+                  (t ;; no splitting; just use the currently selected one
+                   (selected-window))))
+      (select-window sos-answer-view-window)
+      (switch-to-buffer "*sos - answer*")
+      (sos-answer-mode)
 
-(defvar sos-mode-map (make-sparse-keymap) "Keymap used for sos-mode commands.")
-(define-key sos-mode-map "A" 'sos-answer)
+      (make-local-variable 'font-lock-keywords-case-fold-search)
+      (make-local-variable 'font-lock-keywords)
+      (make-local-variable 'font-lock-no-comments)
+      (make-local-variable 'html2text-format-tag-list)
+      (setq font-lock-keywords-case-fold-search t)
+      (setq font-lock-defaults `(sos-answer-font-lock-keywords))
 
-(define-minor-mode sos-mode "Toggle sos-mode.
-With argument ARG turn sos-mode on if ARG is positive, otherwise turn it
-off."
-  :lighter " *SOS*"
-  :group 'org
-  :keymap 'sos-mode-map)
+      ;; (add-to-list 'html2text-format-tag-list '("code" . sos-answer-html2text-code) )
+      (add-to-list 'html2text-format-tag-list '("pre" . sos-answer-html2text-pre) )
 
-(defun sos-mode-on () (interactive)
-  (message "Press A to retrieve SO's answers for the question id at point.")
-  (sos-mode t))
+      (let ((buffer-read-only nil))
+        (erase-buffer)
+        (insert (sos-get-answers id))
+         (html2text)
+        (goto-char (point-min))))))
 
+(defvar sos-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map tabulated-list-mode-map)
+    (define-key map "\C-m" 'sos-answer)
+    map)
+"Keymap used for sos-mode commands.")
+
+(define-derived-mode sos-mode tabulated-list-mode "SOS")
+
+(defface sos-answer-keywords-face
+  '((((class color) (background light)) (:foreground "purple"))
+    (((class color) (background dark)) (:foreground "salmon")))
+  "SOS answer mode face used to highlight keywords."
+  :group 'sos-answer)
+
+(defface sos-answer-code-block-face
+  '((((class color) (background light)) (:foreground "purple"))
+    (((class color) (background dark)) (:foreground "gold")))
+  "SOS answer mode face used to highlight keywords."
+  :group 'sos-answer)
+
+
+(defconst sos-answer-font-lock-keywords
+  (list
+    '("<code>\\([^<]*\\)</code>" . 'sos-answer-keywords-face)
+    )
+  "Sample font face definitions.")
+
+(defun sos-answer-html2text-code (p1 p2 p3 p4)
+  (put-text-property p2 p3 'face 'sos-answer-keywords-face)
+  (html2text-delete-tags p1 p2 p3 p4))
+
+(defun replace-region (from to string)
+  "Replace the region specified by FROM and TO to STRING."
+  (goto-char from)
+  (insert string)
+  (delete-char (- to from)))
+
+(defun sos-answer-html2text-pre (p1 p2 p3 p4)
+  (put-text-property p2 p3 'face 'sos-answer-code-block-face)
+  (replace-region p1 p2 "#+BEGIN_CODE\n")
+  (replace-region p3 p4 "#+END_CODE\n")
+  )
+
+;; in the future?
+;; http://jblevins.org/log/mmm
+
+(defun sos-answer-quit-buffer ()
+  (interactive)
+  (unless (eq major-mode 'sos-answer-mode)
+    (error "Must be in SOS answer mode"))
+  (let ((curbuf (current-buffer)) (curwin (selected-window))
+	 (question-win))
+    (walk-windows
+      (lambda (win)
+	;; check whether the headers buffer window is visible
+	(when (eq sos-question-list-buffer (window-buffer win))
+	  (setq question-win win))
+	;; and kill any _other_ (non-selected) window that shows the current
+	;; buffer
+	(when
+	  (and
+	    (eq curbuf (window-buffer win)) ;; does win show curbuf?
+	    (not (eq curwin win))	    ;; but it's not the curwin?
+	    (not (one-window-p))) ;; and not the last one on the frame?
+	  (delete-window win))))  ;; delete it!
+    ;; now, all *other* windows should be gone.
+    ;; if the headers view is also visible, kill ourselves + window; otherwise
+    ;; switch to the headers view
+    (if (window-live-p question-win)
+      ;; headers are visible
+      (progn
+	(kill-buffer-and-window) ;; kill the view win
+        (setq sos-question-list-window nil)
+	(select-window question-win)) ;; and switch to the headers win...
+      ;; headers are not visible...
+      (progn
+	(kill-buffer)
+        (setq sos-question-list-window nil)
+	(when (buffer-live-p sos-question-list-buffer)
+	  (switch-to-buffer sos-question-list-buffer))))))
+
+(defvar sos-answer-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map special-mode-map)
+    (define-key map "q" 'sos-answer-quit-buffer)
+    map)
+"Keymap used for sos-mode commands.")
+
+(define-derived-mode sos-answer-mode special-mode "SOS Answer")
+
+(add-to-list 'evil-emacs-state-modes 'sos-mode)
+(add-to-list 'evil-emacs-state-modes 'sos-answer-mode)
 
 (provide 'sos)
 
